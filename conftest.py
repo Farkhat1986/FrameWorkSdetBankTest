59a339e33bd0d7fc074c925bf26b0d196331be9c
Farhat <f190886@mail.ru>
2025-03-31 12:59:26 +0300

    create files on project

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+
+@pytest.fixture(scope='module')
+def driver() -> WebDriver:
+    """Фикстура для инициализации WebDrivers.
+
+    Returns:
+        WebDriver: Экземпляр WebDriver для управления браузером.
+    """
+    chrome_options = Options()
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument('--headless=new')
+    driver = webdriver.Chrome(options=chrome_options)
+    yield driver
+    driver.quit()
