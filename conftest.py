c57e13617295f6e0498143947d5e1ad1ad158c0c
Farhat <f190886@mail.ru>
2025-04-04 15:36:07 +0400

    create gitignore

--- conftest.py
+++ conftest.py
@@ -13,8 +13,11 @@ def driver() -> WebDriver:
     """
     chrome_options = Options()
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chrome_options.add_argument("--user-data-dir=C:\\Users\\user\\AppData\\Local\\Google\\Chrome\\User Data")
+    chrome_options.add_argument("--profile-directory=Default")
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument('--headless=new')
     driver = webdriver.Chrome(options=chrome_options)
+
     yield driver
     driver.quit()
